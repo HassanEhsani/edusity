dfe8fdda3406699ad977c118b2ef36f34bd42c2b
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-06 11:30:58 +0300

    i added photo in the assets folder and maked a file navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={logo} alt="" />
+        <ul>
+            <li>Home</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
