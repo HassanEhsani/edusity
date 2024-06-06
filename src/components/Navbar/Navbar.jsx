4130df55565cba1881f052645c8a931d5514ce25
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-06 16:01:48 +0300

    i wrote the css code in the index.css about the btn

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,19 +4,19 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 
 const Navbar = () => {
-  return (
-    <nav>
-        <img src={logo} alt="" />
-        <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About Us</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
-            <li>Contact us</li>
-        </ul>
-    </nav>
-  )
+    return (
+        <nav className='container'>
+            <img src={logo} alt="" className='logo' />
+            <ul>
+                <li>Home</li>
+                <li>Program</li>
+                <li>About Us</li>
+                <li>Campus</li>
+                <li>Testimonials</li>
+                <li><button className='btn'>Contact us</button> </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
